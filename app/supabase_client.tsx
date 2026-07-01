09bc663f3546c31557227425855799a510febe04
Teriot_Tech <120231926+teriotech@users.noreply.github.com>
2026-07-01 13:35:31 +0700

    Update supabase_client.tsx

--- app/supabase_client.tsx
+++ app/supabase_client.tsx
@@ -1,10 +1,10 @@
-import { createBrowserClient } from "@supabase/ssr";
+// import { createBrowserClient } from "@supabase/ssr";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+// const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
-export const createClient = () =>
-  createBrowserClient(
-    supabaseUrl!,
-    supabaseKey!,
-  );
+// export const createClient = () =>
+//   createBrowserClient(
+//     supabaseUrl!,
+//     supabaseKey!,
+//   );
